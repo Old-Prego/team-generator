d27698c54c2b156c8b8caf79c642320414d7050c
Old Prego <spymasterluke@gmail.com>
2021-04-19 18:37:09 -0500

    Second commit, started on index.js

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Manager first
+// ID
+// Name
+// Email
+// Office number
+
+// Engineer or intern?
+// Would you like to add another employee?
+
+// Engineer
+// ID
+// Name
+// Email
+// Github
+
+//Intern
+// ID
+// Name
+// Email
+// School
+
+inquierer
+    .prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the manager's email address?",
+        },
+        
+    ])
